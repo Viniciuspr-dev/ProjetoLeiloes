66731322c905c75796a936febfca9b028ab1d3cb
Vinícius P. Ribeiro <82854424+Viniciuspr-dev@users.noreply.github.com>
2026-08-02 19:47:46 -0300

    Filtro para listar apenas itens vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -14,7 +14,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.sql.SQLException;
 // classe produtoDAO
-
+// classe produtosDAO
 public class ProdutosDAO {
 
     Connection conn;
